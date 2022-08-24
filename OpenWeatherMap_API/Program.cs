3b799226764262c6b290bf132cdd5013f2a63858
Travis Gersch <t_gersch@hotmail.com>
2022-08-23 20:28:03 -0500

    added new question to check if user has api key before running app and exit if not, and added notes explaining app at top.

--- OpenWeatherMap_API/Program.cs
+++ OpenWeatherMap_API/Program.cs
@@ -1,12 +1,26 @@
 ﻿
 
-
+///using openweathermap.org API to check current weather conditions of a user-provided city
+///the first user-provided response is to check whether they have an API key before exiting/continuing
+///upon continuing, user will enter city, then the app will display the current weather conditions
+///in fahrenheit. the app will then ask if the user would like to check another location. if yes, the
+///app will lopp back to entering a city name versus doing the API Key check from beginning. If not, the
+///app will exit.
 
 using Newtonsoft.Json.Linq;
 
 var client = new HttpClient();
-Console.WriteLine("Welcome to the Weather Checker App!");
-Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+Console.WriteLine("Welcome to the Weather Checker App!\n");
+Console.WriteLine("Please be advised you will need an API Key from http://openweathermap.org to use this app. Do you have one?\n");
+Console.Write("Yes/No:   ");
+var keyCheck = Console.ReadLine().ToLower();
+if(keyCheck == "no" || keyCheck == "n")
+{
+    Console.WriteLine("I'm sorry. Please re-run the app when you have an API Key. Have a great day!");
+    return;
+}
+
+Console.WriteLine("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 Console.Write("Please enter your API access key:");
 var apiKey = Console.ReadLine();
 
@@ -30,6 +44,7 @@ while (true)
 
     if (exitCue == "n" || exitCue == "no")
     {
+        Console.WriteLine("Thanks for checking me out! Have a great day!");
         break;
     }
 }
